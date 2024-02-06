be99b14eb728cb275285c75d0d6d9800b00e6f8f
Shoitii <shoiti.isotani@gmail.com>
2024-02-05 21:46:00 -0300

    jokenpo updated

--- src/jokenpo_game.py
+++ src/jokenpo_game.py
@@ -1,70 +1,29 @@
 import random
 
-def ganhar():
-    print(f'Ganhou!Você escolheu {py} e eu {com}')
-def perder():
-    print(f'Perdeu!Você escolheu {py} e eu {com}')
-def empate():
-    print(f'Empate! Os dois escolheram {com}')
-
-
-loop = True
-lista = ['pedra', 'papel', 'tesoura']
-py = 0
-count_vitorias = 0
-count_derrotas = 0
-count_empates = 0
-print('\033[33m=-=\033[m' * 20)
-print('\033 JOKENPÔ GAME\033[m')
-print('\033[33m=-=\033[m' * 20)
-while loop:
-    print('''[ 1 ] pedra
-[ 2 ] papel
-[ 3 ] tesoura''')
-    py = int(input('>>> Escolha: '))
-    print('\033[33m=-=\033[m' * 20)
-    com = random.choice(lista).lower()
-    if py == 1:
-        py = 'pedra'
-        if com == 'tesoura':
-            ganhar()
-            count_vitorias += 1
-        elif com == 'papel':
-            perder()
-            count_derrotas += 1
-        elif com == py:
-            empate()
-            count_empates += 1
-    elif py == 2:
-        py = 'papel'
-        if com == 'pedra':
-            ganhar()
-            count_vitorias += 1
-        elif com == 'tesoura':
-            perder()
-            count_derrotas += 1
-        elif com == py:
-            empate()
-            count_empates += 1
-    elif py == 3:
-        py = 'tesoura'
-        if com == 'papel':
-            ganhar()
-            count_vitorias += 1
-        elif com == 'pedra':
-            perder()
-            count_derrotas += 1
-        elif com == py:
-            empate()
-            count_empates += 1
+def play():
+  count_wins = count_ties = count_losts = 0
+  while True:   
+    player = input("Type 'r' for rock, 'p' for paper, 's' scissors and '0' to exit: ")
+    comp = random.choice(['r','p','s'])
+    if player == '0':
+      print('Thanks for playing, see you next time!')
+      print('-==-==- SCORE -==-==-')
+      print(f'Wins:{count_wins}  Losts:{count_losts}  Ties:{count_ties}')
+      break
+    elif player == comp:
+      count_ties += 1
+      print('Tie')
+    elif win(player, comp):
+      count_wins += 1
+      print('You won!')
     else:
-        print('Erro, tente novamente!')
-    loop = input('Jogar novamente? [S/N] ').strip().lower()
-    print('\033[33m=-=\033[m' * 20)
-    if loop == 'n':
-            loop = False
-print('SUA PONTUAÇÃO FINAL')
-print('\033[33m=-=\033[m'*20)
-print(f'Vitórias: {count_vitorias}')
-print(f'Derrotas: {count_derrotas}')
-print(f'Empates: {count_empates}')
+      count_losts += 1
+      print('You lost!')
+
+def win(player,opponent):
+  if (player=='r' and opponent=='s') or (player=='s' and opponent=='p') or (player=='p' and opponent=='r'):
+    return True
+
+
+print("JOKENPO GAME")
+play()
